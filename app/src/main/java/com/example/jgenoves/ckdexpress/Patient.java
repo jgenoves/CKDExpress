474519845f8844d5eb76f07b869f8cb791aba142
Jordan.Genovese <jordan.genovese@uvm.edu>
2019-11-13 00:10:26 -0500

    Working Recycler View, need to bind patients info to each entry in the recycler view and style each list item

--- app/src/main/java/com/example/jgenoves/ckdexpress/Patient.java
+++ app/src/main/java/com/example/jgenoves/ckdexpress/Patient.java
@@ -1,34 +1,63 @@
 package com.example.jgenoves.ckdexpress;
 
+import android.content.Context;
+
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 public class Patient {
 
+    private static Patient sPatient;
 
-    private String mName;
 
+    private String mFirstName;
+    private String mLastName;
     private List<EGFREntry> mGfrScores;
     private double mBaseGFRLevel;
 
     private boolean mCheckupDue;
     private boolean mNephVisitDue;
 
+    public static Patient get(Context context){
+        if(sPatient == null){
+            sPatient = new Patient(context);
+        }
+        return sPatient;
+    }
 
-    public Patient(){
-        mName = "";
-        mGfrScores = null;
-        mBaseGFRLevel = 0.0;
+
+    public Patient(Context context){
+        mFirstName = "Jordan";
+        mLastName= "Genovese";
+        mGfrScores = new ArrayList<EGFREntry>();
+        mBaseGFRLevel = 50;
         mCheckupDue = false;
         mNephVisitDue = false;
+
+
+        for(int i = 0; i < 10; i++){
+            EGFREntry e = new EGFREntry();
+            e.setScore(50);
+            mGfrScores.add(e);
+        }
     }
 
-    public String getName() {
-        return mName;
+    public String getFirstName() {
+        return mFirstName;
     }
 
-    public void setName(String name) {
-        mName = name;
+    public void setFirstName(String name) {
+        mFirstName = name;
+    }
+
+    public String getLastName() {
+        return mLastName;
+    }
+
+    public void setLastName(String name) {
+        mLastName = name;
     }
 
     public List<EGFREntry> getGfrScores() {
@@ -63,6 +92,21 @@ public class Patient {
         mNephVisitDue = nephVisitDue;
     }
 
+
+
+    public void addGFRScore(EGFREntry e){
+        mGfrScores.add(e);
+    }
+
+    public EGFREntry getGFRScore(UUID id){
+        for(EGFREntry e:mGfrScores){
+            if(e.getId().equals(id)){
+                return e;
+            }
+
+        }
+        return null;
+    }
 
 
 
