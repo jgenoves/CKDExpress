2e090405d2e1dc4106850af7debb347c394ac8cf
will <willfath12345@gmail.com>
2019-11-29 00:22:15 -0500

    The notifications page now displays different stuff depending on what is going on with the GFR. it's not yet implemented with the eGFR class but it will be as soon as everything comes together

--- app/src/main/java/com/example/jgenoves/ckdexpress/notificationFragment.java
+++ app/src/main/java/com/example/jgenoves/ckdexpress/notificationFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.graphics.Color;
 
 import org.w3c.dom.Text;
 
@@ -14,10 +15,8 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
 public class notificationFragment extends Fragment {
-    //this was all copied from the homepage fragment class
-    //going forward, I'll edit it so it actually fits for the class
-    //TODO::Finish this class and create an XML
-    //XML created still ned to flesh it out and make all of the logic
+    /*TODO:: Base notifications are pretty much done now! just a little bit of fixing uo
+    *  and making things look better, still need to implement the */
     private Patient mPatient;
     private View mLineBreak_1;
     private TextView mWelcome;
@@ -42,19 +41,54 @@ public class notificationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         //need to mkae up fragment notification
         View v = inflater.inflate(R.layout.fragment_notifications, container, false);
-
+        testGFR = 4;
 
 //        mLineBreak_1 = (View) v.findViewById(R.id.line_break1);
 //
 //        mWelcome = (TextView) v.findViewById(R.id.welcome_text);
 //        mWelcome.setText("Welcome back, \n" + mPatient.getFirstName() + ".");
 
-        mNotificationTitle = (TextView) v.findViewById(R.id.notification_title);
-        mNotificationTitle.setText("Alert Title test");
-        mNotificationMessages = (TextView) v.findViewById(R.id.notification_messages);
-        mNotificationMessages.setText("im just gonn type in a bunch of stuff here\n" +
-                "there will eventually be way more and it will change depending\n " +
-                "on the level of the GFR, just printing this for now");
+
+        if(testGFR == 1){
+            mNotificationTitle = (TextView) v.findViewById(R.id.notification_title);
+            mNotificationTitle.setText("No Current Notifications");
+            mNotificationMessages = (TextView) v.findViewById(R.id.notification_messages);
+            mNotificationMessages.setText("Everything is looking good!");
+
+            mNotificationMessages.setTextColor(Color.DKGRAY);
+            mNotificationTitle.setTextColor(Color.GREEN);
+        }else if(testGFR == 2){
+            mNotificationTitle = (TextView) v.findViewById(R.id.notification_title);
+            mNotificationTitle.setText("Schedule an appointment with your doctor soon");
+            mNotificationMessages = (TextView) v.findViewById(R.id.notification_messages);
+            mNotificationMessages.setText("It looks like your GFR level has changed a bit" +
+                    " too much, you should schedule an appointment with your" +
+                    " doctor within the next month");
+
+            mNotificationMessages.setTextColor(Color.DKGRAY);
+            //TODO:: make any/all of these colors statics in XML
+            mNotificationTitle.setTextColor(Color.rgb(199, 79, 23));
+        }else if (testGFR == 3) {
+            mNotificationTitle = (TextView) v.findViewById(R.id.notification_title);
+            mNotificationTitle.setText("See your doctor ASAP");
+            mNotificationMessages = (TextView) v.findViewById(R.id.notification_messages);
+            mNotificationMessages.setText("Your GFR levels have changed a bit too much!" +
+                    " You should contact your doctor immediately to make sure " +
+                    " everything is alright!");
+
+            mNotificationMessages.setTextColor(Color.DKGRAY);
+            mNotificationTitle.setTextColor(Color.rgb(207, 33, 36));
+        } else{
+            //there has been an error with the gfr levelsmNotificationTitle = (TextView) v.findViewById(R.id.notification_title);
+            mNotificationTitle = (TextView) v.findViewById(R.id.notification_title);
+            mNotificationTitle.setText("Notification Error");
+            mNotificationMessages = (TextView) v.findViewById(R.id.notification_messages);
+            mNotificationMessages.setText("You shoudln't ever see this, if you do" +
+                    " that means there is an error on our end");
+
+            mNotificationMessages.setTextColor(Color.CYAN);
+            mNotificationTitle.setTextColor(Color.CYAN);
+        }
 
 
 //        mNavButton = (Button) v.findViewById(R.id.to_egfr_scores_button);
