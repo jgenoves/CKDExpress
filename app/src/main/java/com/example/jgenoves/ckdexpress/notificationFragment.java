b1798f2057655187e00a4e117e36b65a4e6fcb60
will <willfath12345@gmail.com>
2019-11-28 19:18:54 -0500

    Notifications page works, right now displays test values. Soon will implement the logic to switch it and format it so it looks like a proper notification tab

--- app/src/main/java/com/example/jgenoves/ckdexpress/notificationFragment.java
+++ app/src/main/java/com/example/jgenoves/ckdexpress/notificationFragment.java
@@ -17,56 +17,65 @@ public class notificationFragment extends Fragment {
     //this was all copied from the homepage fragment class
     //going forward, I'll edit it so it actually fits for the class
     //TODO::Finish this class and create an XML
-
+    //XML created still ned to flesh it out and make all of the logic
     private Patient mPatient;
     private View mLineBreak_1;
     private TextView mWelcome;
-    private TextView mAlertTitle;
-    private TextView mAlertMessages;
+    private TextView mNotificationTitle;
+    private TextView mNotificationMessages;
     private Button mNavButton;
     private Button mNotificationButton;
+
+    //this will be set to 1, 2, 3 for testing purposes
+    //this will eventually be grabbed from the patient class
+    private int testGFR;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-
-        mPatient = Patient.get(getActivity());
+//
+//        mPatient = Patient.get(getActivity());
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        //need to mkae up fragment notification
+        View v = inflater.inflate(R.layout.fragment_notifications, container, false);
 
 
-        mLineBreak_1 = (View) v.findViewById(R.id.line_break1);
+//        mLineBreak_1 = (View) v.findViewById(R.id.line_break1);
+//
+//        mWelcome = (TextView) v.findViewById(R.id.welcome_text);
+//        mWelcome.setText("Welcome back, \n" + mPatient.getFirstName() + ".");
 
-        mWelcome = (TextView) v.findViewById(R.id.welcome_text);
-        mWelcome.setText("Welcome back, \n" + mPatient.getFirstName() + ".");
-
-        mAlertTitle = (TextView) v.findViewById(R.id.alert_title);
-        mAlertMessages = (TextView) v.findViewById(R.id.alert_messages);
-
-
-        mNavButton = (Button) v.findViewById(R.id.to_egfr_scores_button);
-        mNavButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                Intent intent = eGFRListActivity.newIntent(getActivity());
-                startActivity(intent);
+        mNotificationTitle = (TextView) v.findViewById(R.id.notification_title);
+        mNotificationTitle.setText("Alert Title test");
+        mNotificationMessages = (TextView) v.findViewById(R.id.notification_messages);
+        mNotificationMessages.setText("im just gonn type in a bunch of stuff here\n" +
+                "there will eventually be way more and it will change depending\n " +
+                "on the level of the GFR, just printing this for now");
 
 
-            }
-        });
+//        mNavButton = (Button) v.findViewById(R.id.to_egfr_scores_button);
+//        mNavButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v){
+//                Intent intent = eGFRListActivity.newIntent(getActivity());
+//                startActivity(intent);
+//
+//
+//            }
+//        });
 
-        mNotificationButton = (Button) v.findViewById(R.id.to_notifications_button);
-        mNotificationButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent = notificationActivity.newIntent(getActivity());
-                startActivity(intent);
-            }
-        });
+//        mNotificationButton = (Button) v.findViewById(R.id.to_notifications_button);
+//        mNotificationButton.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v){
+//                Intent intent = notificationActivity.newIntent(getActivity());
+//                startActivity(intent);
+//            }
+//        });
 
         return v;
     }
